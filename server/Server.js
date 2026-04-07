e1431f38dcbae81cebd4e48b96300ac454582804
Vijaya Raghavendra <kummarivijay2007@gmail.com>
2026-04-07 21:50:15 +0530

    fixed db module error

--- server/Server.js
+++ server/Server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const connectDB = require("./DB");
+const connectDB = require("./Db");
 
 const app = express();
 
